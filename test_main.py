fc76e06cb94756f6f05a4d5b91bb7e07b505f1b7
canelajoel3 <canelajoel3@gmail.com>
2026-08-18 17:15:10 -0600

    Añadiendo prueba a la funcion de Multiplicar

--- test_main.py
+++ test_main.py
@@ -8,4 +8,6 @@ class TestCaluladora:
 
     def test_subtraction(self):
         assert 2 == main.subtract(4,2)
-        
+
+    def test_multiplication(self):
+        assert 100 == main.multiply(10, 10)
